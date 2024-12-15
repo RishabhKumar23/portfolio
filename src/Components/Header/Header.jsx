900b6830e4b7754a68f205a9b5dea5acc22442e7
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2024-12-15 20:10:39 +0000

    header change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className='container header_container'>
         <h5>Hello I'm</h5>
         <h1>Rishabh Kumar</h1>
-        <h5 className='text-light'>software engineering </h5>
+        <h5 className='text-light'>Software Engineer</h5>
         <Cv />
         <Socials />
         <div className='profile'>
